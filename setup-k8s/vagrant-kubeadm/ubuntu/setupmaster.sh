c2d7dae33dcddadcf707822e994ffd5bfb563482
snorics <snorics@gmail.com>
2022-10-27 01:33:38 +0200

    Update setupmaster.sh

--- setup-k8s/vagrant-kubeadm/ubuntu/setupmaster.sh
+++ setup-k8s/vagrant-kubeadm/ubuntu/setupmaster.sh
@@ -41,6 +41,8 @@ kubectl get pod -n rook-ceph
 # curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 # chmod 700 get_helm.sh
 # ./get_helm.sh
+#helm repo add bitnami https://charts.bitnami.com/bitnami
+#helm install my-release bitnami/mysql
 
 
 #kubectl apply -f rbd/storageclass.yaml
