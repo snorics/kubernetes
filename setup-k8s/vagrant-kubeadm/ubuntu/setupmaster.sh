fc1a8e65ce5a62db9153fa770085a04e0d9f5db6
snorics <snorics@gmail.com>
2022-10-26 22:08:06 +0200

    Update setupmaster.sh

--- setup-k8s/vagrant-kubeadm/ubuntu/setupmaster.sh
+++ setup-k8s/vagrant-kubeadm/ubuntu/setupmaster.sh
@@ -5,10 +5,10 @@ kubeadm config images pull >/dev/null 2>&1
 
 echo "[TASK 2] Initialize Kubernetes Cluster"
 #kubeadm init --apiserver-advertise-address=172.16.16.100 --pod-network-cidr=192.168.0.0/16 >> /root/kubeinit.log 2>/dev/null
-kubeadm init --pod-network-cidr 10.244.0.0/16 --apiserver-advertise-address=192.168.56.2>> /root/kubeinit.log
+kubeadm init --pod-network-cidr 10.244.0.0/16 --apiserver-advertise-address=192.168.178.37>> /root/kubeinit.log
 
 echo "[TASK 3] Deploy Calico network"
-kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.18/manifests/calico.yaml >/dev/null 2>&1
+kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.24.3/manifests/calico.yaml >/dev/null 2>&1
 
 # echo "[TASK 3] Deploy Weave network"
 # kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
